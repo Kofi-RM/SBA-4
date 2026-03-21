981a08cf7e9837c5f2e85d6854fab126ad2506bb
Kofi-RM <kofireevesmiller@gmail.com>
2026-03-21 06:23:36 -0400

    Added change status on button click

--- script.js
+++ script.js
@@ -7,23 +7,72 @@ let date = document.getElementById("date");
 let statusFilter = document.getElementById("statusSelect")
 let categoryFilter = document.getElementById("categorySelect")
 let categoryOptions = new Set();
-let items = [{task:"Go to store", category:"Errand", date:"2026-03-21"} 
-   , {task:"Walk the cat", category:"Errand", date:"2026-03-23"} ,
-   {task:"Go to bar", category:"Leisure", date:"2026-03-30"}
+let items = [{task:"Go to store", category:"Errand", date:"2026-03-21", status:"In Progress"} 
+   , {task:"Walk the cat", category:"Errand", date:"2026-03-23", status:"In Progress"} ,
+   {task:"Go to bar", category:"Leisure", date:"2026-03-30", status:"In Progress"}
 ];
 
 
+window.onclick = function(event) {
+    if (event.target.matches('[name="option"]')) {
+        console.log("we outside")
+        var dropdowns = document.getElementsByName("updateStatus");
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+if (openDropdown.classList.contains('show')) {
+    openDropdown.classList.remove('show');
+}
+        }
+    }
+}
+
 let changeStatus = (e) => {
     let p = e.target.closest("div");
-    console.log(p);
+    let d = p.querySelector("div")
+    let select = d.querySelector("select")
+    console.log(select)
+    console.log(d);
+    select.addEventListener("change", () => {
+    let id = Number(select.id);
+    console.log("id:", id);
+console.log("item at id:", items[id]);
+    console.log(items[id].status)
+    
+    console.log(select.value)
+     items[id].status = select.value;
+    console.log(items);
+   
+    showItems();
+
+    
+    })
+    select.classList.add("show");
+
+
+
+
 }
-let createItems = (item) => {
+let createItems = (item, count) => {
+    
  let listItem = document.createElement("li");
         let taskText = document.createElement("p");
         let categoryText = document.createElement("p");
         let dateText = document.createElement("p");
         let statusText = document.createElement("p");
+        let dropdown = document.createElement("div");
+        dropdown.innerHTML =   `<select>
 
+    <option>In Progress</option>
+    <option>Completed</option>
+    <option>Overdue</option>
+   </select>`
+
+        let select =  dropdown.querySelector("select");
+        select.classList.add("hide");
+       select.id = count;
+         select.name = "updateStatus";
+        
+         statusText.id = `status${count}`
         let statusButton = document.createElement("button");
         statusButton.textContent = "Change status";
         statusButton.classList.add("status-button");
@@ -47,15 +96,16 @@ let createItems = (item) => {
         taskText.textContent = "Task: " +item.task;
         categoryText.textContent = "Category: " + item.category;
         dateText.textContent = "Date: " + item.date;
+        statusText.textContent = item.status;
         // setting text content
 
-const today = new Date();
-const formattedToday = today.toISOString().split("T")[0];
 
+    
+select.addEventListener("change", (e) => {
+    console.log("selected:", e.target.value);
+    select.classList.remove("show");
+});
 
-statusText.textContent = item.date >= formattedToday ? "In Progress" : "Overdue";
-item.status = statusText.textContent;
-// Sets status to Overdue or In progress depending on date
 
 
         taskContainer.appendChild(taskText);
@@ -65,6 +115,7 @@ item.status = statusText.textContent;
           flexbox.appendChild(taskContainer);
           statusContainer.appendChild(statusText);
           statusContainer.appendChild(statusButton);
+          statusContainer.appendChild(dropdown);
           flexbox.appendChild(statusContainer);
 
           listItem.appendChild(flexbox);
@@ -80,9 +131,10 @@ let updateCategory = (category) => {
 } // adds category to dropdown
 
 let showItems = () => {
+      list.innerHTML ="";
     let categories = categoryFilter.children;
     let update = new Set();
-    console.log(categories);
+  
     
     
     
@@ -94,9 +146,10 @@ let showItems = () => {
     } else if (statusFilter != "Off") {
         filterItems = items.filter((item) => item.status == statusFilter.value )
     }
-
+    let count = 0;
     for (let item of filterItems) {
-      createItems(item);
+      createItems(item, count);
+      count++;
       categoryOptions.add(item.category);
     } // create listItem for each item
 
@@ -118,12 +171,12 @@ let showItems = () => {
 
 showItems();
 statusFilter.addEventListener("change", () => {
-    list.innerHTML ="";
+  
     showItems();
 })
 
 categoryFilter.addEventListener("change", () => {
-    list.innerHTML ="";
+  
     showItems();
 })
 submitButton.addEventListener("click", () => {
@@ -131,6 +184,14 @@ submitButton.addEventListener("click", () => {
     item.task = task.value;
     item.category = category.value;
     item.date = date.value;
+
+    const today = new Date();
+const formattedToday = today.toISOString().split("T")[0];
+
+
+let status = item.date >= formattedToday ? "In Progress" : "Overdue";
+item.status = status;
+// Sets status to Overdue or In progress depending on date
 
     items.push(item);
     console.log(item);
