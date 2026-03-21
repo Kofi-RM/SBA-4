baebf16d401a3079dd34f4395745780082bd9287
Kofi-RM <kofireevesmiller@gmail.com>
2026-03-21 12:01:24 -0400

    All required functionality

--- script.js
+++ script.js
@@ -7,22 +7,8 @@ let date = document.getElementById("date");
 let statusFilter = document.getElementById("statusSelect")
 let categoryFilter = document.getElementById("categorySelect")
 let categoryOptions = new Set();
-let items = JSON.parse(localStorage.getItem("items"))
-
-
-
-window.onclick = function(event) {
-    if (event.target.matches('[name="option"]')) {
-        console.log("we outside")
-        var dropdowns = document.getElementsByName("updateStatus");
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-if (openDropdown.classList.contains('show')) {
-    openDropdown.classList.remove('show');
-}
-        }
-    }
-}
+let items = JSON.parse(localStorage.getItem("items")) || [];
+;
 
 let changeStatus = (e) => {
     let p = e.target.closest("div");
@@ -32,21 +18,70 @@ let changeStatus = (e) => {
     select.addEventListener("change", () => {
     let id = Number(select.id);
 
-    
-    console.log(select.value)
-     items[id].status = select.value;
-    
+    let i = localStorage.getItem("items");
+    let itemsParse = JSON.parse(i);
+    // get local storage
+
+    console.log(itemsParse[id])
+     itemsParse[id].status = select.value;
+     // change valuei in local storage
+    localStorage.setItem("items", JSON.stringify(itemsParse))
    
     showItems();
-
+    // update
     
     })
     select.classList.add("show");
-
-
-
-
 }
+
+let updateCategory = (category) => {
+        let newCategory = document.createElement("option");
+    newCategory.innerHTML =  category;
+    categoryFilter.appendChild(newCategory);
+} // adds category to dropdown
+
+let showItems = () => {
+    let jsonItems = JSON.parse(localStorage.getItem("items"));
+    console.log(jsonItems)
+      list.innerHTML ="";
+    let categories = categoryFilter.children;
+    
+    let filterItems;
+    if (statusFilter.value == "Off" && categoryFilter.value == "Off") {
+        filterItems = jsonItems;
+    } else if (categoryFilter.value == "Off") {
+     filterItems = jsonItems.filter((item) =>item.status == statusFilter.value )
+    } else if (statusFilter.value == "Off") {
+        filterItems = jsonItems.filter((item) => item.category == categoryFilter.value )
+    } else {
+        filterItems = jsonItems.filter((item) => item.status == statusFilter.value && item.category == categoryFilter.value)
+        
+    }
+
+    let count = 0;
+    if (filterItems) {
+    for (let item of filterItems) {
+      createItems(item, count);
+      count++;
+      if (item.category != "") {
+      categoryOptions.add(item.category);
+      }
+    } // create listItem for each item
+    }
+    let currentCategories = new Set();
+
+    for (let i = 0; i < categories.length; i++) {
+        currentCategories.add(categories[i].value);
+    }
+
+    let addCategories = categoryOptions.difference(currentCategories);
+
+    for (let category of addCategories) {
+    updateCategory(category);
+}
+}
+
+
 let createItems = (item, count) => {
     
  let listItem = document.createElement("li");
@@ -56,7 +91,7 @@ let createItems = (item, count) => {
         let statusText = document.createElement("p");
         let dropdown = document.createElement("div");
         dropdown.innerHTML =   `<select>
-
+    <option></option>
     <option>In Progress</option>
     <option>Completed</option>
     <option>Overdue</option>
@@ -82,7 +117,7 @@ let createItems = (item, count) => {
         flexbox.classList.add("flex-box");
 
         let taskContainer = document.createElement("div");
-    //    taskContainer.classList.add("flex-box")
+  
 
     let statusContainer = document.createElement("div");
     statusContainer.classList.add("flex-box")
@@ -97,9 +132,9 @@ let createItems = (item, count) => {
 
     
 select.addEventListener("change", (e) => {
-    console.log("selected:", e.target.value);
+
     select.classList.remove("show");
-});
+}); // remove dropdown after selecting
 
 
 
@@ -119,66 +154,18 @@ select.addEventListener("change", (e) => {
           // connecting elements to each other
 }
 
-let updateCategory = (category) => {
-        let newCategory = document.createElement("option");
-    newCategory.innerHTML =  category;
-    categoryFilter.appendChild(newCategory);
-} // adds category to dropdown
-
-let showItems = () => {
-    let jsonItems = JSON.parse(localStorage.getItem("items"));
-    console.log(jsonItems)
-      list.innerHTML ="";
-    let categories = categoryFilter.children;
-    let update = new Set();
-  
-    
-    
-    
-    let filterItems;
-    if (statusFilter.value == "Off" && categoryFilter.value == "Off") {
-        filterItems = jsonItems;
-    } else if (categoryFilter.value == "Off") {
-     filterItems = jsonItems.filter((item) =>item.status == statusFilter.value )
-    } else if (statusFilter == "Off") {
-        filterItems = jsonItems.filter((item) => item.category == categoryFilter.value )
-    } else {
-        filterItems = jsonItems.filter((item) => item.status == statusFilter.value && item.category == categoryFilter.value)
-        
-    }
-    let count = 0;
-    for (let item of filterItems) {
-      createItems(item, count);
-      count++;
-      categoryOptions.add(item.category);
-    } // create listItem for each item
-
-    let currentCategories = new Set();
-
-    for (let i = 0; i < categories.length; i++) {
-        currentCategories.add(categories[i].value);
-    }
-
-    let addCategories = categoryOptions.difference(currentCategories);
-
-    for (let category of addCategories) {
-    updateCategory(category);
-}
-}
-
 
 
 
 
 statusFilter.addEventListener("change", () => {
-  
     showItems();
 })
 
 categoryFilter.addEventListener("change", () => {
-  
     showItems();
 })
+
 submitButton.addEventListener("click", () => {
     let item = {};
     item.task = task.value;
@@ -193,6 +180,8 @@ let status = item.date >= formattedToday ? "In Progress" : "Overdue";
 item.status = status;
 // Sets status to Overdue or In progress depending on date
 
+    console.log(items);
+    console.log(item)
     items.push(item);
     console.log(items);
     console.log(JSON.stringify(items));
@@ -201,7 +190,7 @@ let storedItems = JSON.parse(localStorage.getItem("items"));
 console.log("Stored items:", storedItems);
 
     categoryOptions.add(item.category);
-    localStorage.setItem("category", categoryOptions);
+
 
     task.value= "";
     category.value = "";
